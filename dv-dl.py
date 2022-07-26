8a01aa13a34ea3cce1e73753c8ccba3bf96ab110
eric <starwolf64.exe@gmail.com>
2022-07-26 13:50:24 -0500

    Downloads work and added the ability to unzip

--- dv-dl.py
+++ dv-dl.py
@@ -4,9 +4,11 @@ import pprint
 import os
 import re
 import sys #sys.exit()
+import zipfile
 
 base = 'https://dataverse.unc.edu'
 search_term = 'Harris+1973+Nuclear+Power+Survey+study+no+2345'
+unzip = True
 
 # Read in API Key
 try:
@@ -17,9 +19,7 @@ except IOError:
 
 API_key = api_file.readlines()
 api_file.close()
-API_key_html = "&key=" + API_key 
-print(API_key[0])
-sys.exit()
+API_key_html = "&key=" + API_key[0]
 
 def create_dir_if_not_exist(path):
     if not os.path.exists('path'):
@@ -29,7 +29,7 @@ def create_dir_if_not_exist(path):
             #print(error)
             pass
 
-
+# Initalize parent directory
 def init_dir():
     parent_dir = os.getcwd()
     path = os.path.join(parent_dir, "dataverse_datasets")
@@ -38,18 +38,28 @@ def init_dir():
     
         
 ##Download to folder
-def download(dir_name, global_id, db_id):
+def download(dir_name, global_id):
     #create the sub dir
     parent_dir = os.getcwd()
     parent_dir = os.path.join(parent_dir, "dataverse_datasets")
-    path = os.path.join(parent_dir, dir_name)
-    create_dir_if_not_exist(path)
-    
-    #download the file
-    #os.chdir(path)
-    dl_url = base + '/api/access/dataset/:persistentId?persistentId=' + global_id
-    print(dl_url)
-    
+    dl_path = os.path.join(parent_dir, dir_name)
+    create_dir_if_not_exist(dl_path)
+    file_path = dl_path + "/dataverse_files.zip"
+
+    #check if the files is already downloaded
+    if os.path.exists(file_path):
+        print("File already exists")
+    else:
+        #download the file
+        dl_url = base + '/api/access/dataset/:persistentId?persistentId=' + global_id
+        #print(dl_url)
+        #print(dl_path)
+        os.system("wget -nc --content-disposition -P '{}' '{}'".format(dl_path, dl_url))
+        #print("wget --content-disposition -P '{}' '{}'".format(dl_path, dl_url))
+        if unzip:
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                extract_path = os.path.join(dl_path + "/extract/")
+                zip_ref.extractall(extract_path)
 
 
 init_dir()
@@ -73,10 +83,10 @@ while (condition):
         #print(i['entity_id'])
         #print("global_id:", i['global_id'])
         #print("url:", i['url'])
-        download(i['name'], i['global_id'], i['entity_id'])
+        download(i['name'], i['global_id'])
         pass
     start = start + rows
     page += 1
     condition = start < total
-    #For testing only
+    #For testing only and artificial limiter
     condition = False
